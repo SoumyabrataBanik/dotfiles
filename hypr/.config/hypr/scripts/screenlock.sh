666b2cd38c7b4ab0dcf5476496a45c14505d1d38
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2024-06-26 09:39:26 +0530

    Modularize the configuration of hyprland

--- hypr/.config/hypr/scripts/screenlock.sh
+++ hypr/.config/hypr/scripts/screenlock.sh
@@ -12,10 +12,16 @@ if [ -f /tmp/screenlock/blur.png ]; then
     rm /tmp/screenlock/blur.png
 fi
 
+if [ -f /tmp/screenlock/prelock.png ]; then
+    rm /tmp/screenlock/prelock.png
+fi
+
 grim /tmp/screenlock/current.png
 
 convert -scale 10% -blur 0x4 -resize 1000% /tmp/screenlock/current.png /tmp/screenlock/blur.png
 
-composite -gravity southeast ~/dotfiles/wallpapers/over.png /tmp/screenlock/blur.png /tmp/screenlock/lock.png
+composite -gravity southeast ~/dotfiles/wallpapers/overlay1.png /tmp/screenlock/blur.png /tmp/screenlock/prelock.png
+
+composite -gravity southwest ~/dotfiles/wallpapers/overlay2.png /tmp/screenlock/prelock.png /tmp/screenlock/lock.png
 
 hyprlock
